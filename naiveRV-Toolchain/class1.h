6160df3b242dfdb01a53a3e80d63047bb4f7fa35
Kai Horstmann <horstmannkai@hotmail.com>
2023-03-01 08:43:28 +0100

    WIP

--- naiveRV-Toolchain/class1.h
+++ naiveRV-Toolchain/class1.h
@@ -2,14 +2,21 @@
 class class1 {
     
 public:
+    class1() {
+        val = 0.0;
+    }
+
+    virtual ~class1() {
+    }
+
     static void doSomething();
     
     static double getVal();
     
 private:
-    
+
     static class1 theOneAndOnly;
     
-    double val = 0.0;
+    double val;
 };
         
